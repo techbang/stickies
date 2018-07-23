4f9d01e11f1813c16cf262ee70d50b6ffd27400e
shangwei <shangweilin7@gmail.com>
2018-07-23 16:34:15 +0800

    add new custom message

--- lib/stickies/access_helpers.rb
+++ lib/stickies/access_helpers.rb
@@ -10,10 +10,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -50,13 +50,18 @@ module Stickies
       Stickies::Messages.debug(session, message, options)
     end
 
+    # Add a custom notify message.  See Stickies::Messages#add for full details.
+    def notify_stickie(message, options={})
+      Stickies::Messages.notify(session, message, options)
+    end
+
     ################################################################################
     # Check to see if a message has been seen by the user.  See
     # Stickies::Messages#seen? for full details.
     def stickie_seen? (name, options={})
       Stickies::Messages.seen?(session, name, options)
     end
-    
+
   end
 end
 ################################################################################
