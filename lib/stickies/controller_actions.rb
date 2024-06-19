15eb8caa3593696eb48d13dfdd4218816e1ff998
bunny <bunny_wei@hmg.com.tw>
2024-06-19 13:56:49 +0800

    alias_method_chain to alias_method

--- lib/stickies/controller_actions.rb
+++ lib/stickies/controller_actions.rb
@@ -10,10 +10,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -25,41 +25,23 @@
 ################################################################################
 module Stickies
   module ControllerActions
-
-    ################################################################################
-    # Add some magic to ActionController::Base (and all controllers)
-    def self.included (base)
-      # The call_filter method was renamed to call_filters after version 1.2.3
-      # was released, but the change is on the 1.2 stable branch.  This code
-      # will insert a backwards compatibly layer for us
-      if !base.public_instance_methods.include?("call_filter")
-        class_eval("def call_filter (*a, &b) call_filters(*a, &b) end")
-      end
-
-      base.class_eval do
-        alias_method_chain(:call_filter, :stickie_check)
+    module StickieCheck
+      def call_filter(chain, index)
+        if action_name == 'destroy_stickie'
+          action_methods << 'destroy_stickie'
+          perform_action_without_filters
+        else
+          super
+        end
       end
     end
 
-    ################################################################################
-    # Since the destroy_stickie action only changes the session object, don't
-    # use any filters when calling it.  This might be a bit like smashing
-    # a bug with a sledgehammer, but it's pretty effective.
-    #
-    # Without this magic, filters will interfere with this very simple action,
-    # and users would have to use:
-    #
-    #  before_filter(x, :exclude => :destroy_stickie)
-    #
-    # Each time a filter is used.  So, I decided that this is a much better
-    # option.  Let me know if you have a better idea.
-    def call_filter_with_stickie_check (chain, index)
-      if action_name == 'destroy_stickie'
-        action_methods << 'destroy_stickie'
-        perform_action_without_filters
-      else
-        call_filter_without_stickie_check(chain, index)
+    def self.included(base)
+      if !base.public_instance_methods.include?("call_filter")
+        base.class_eval("def call_filter (*a, &b) call_filters(*a, &b) end")
       end
+
+      base.prepend StickieCheck
     end
 
     ################################################################################
@@ -68,7 +50,6 @@ module Stickies
       Stickies::Messages.fetch(session) {|m| m.destroy(params[:id])}
       render(:nothing => true)
     end
-
   end
 end
 ################################################################################
